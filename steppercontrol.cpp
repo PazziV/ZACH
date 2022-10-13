6b580da1bba109ab32da90fc56d3cbf0fb8e7be5
ZACH <pasiveselka11@gmail.com>
2022-10-13 08:10:26 +0100

    misery

--- steppercontrol.cpp
+++ steppercontrol.cpp
@@ -15,12 +15,22 @@ using namespace std;
 #define M2_STEP 13     //Stepper 2 Step Pin
 #define M2_DIR 19     //Stepper 2 Direction Pin
 
+#define LIMIT_X_LEFT 20  //left Limit Switch X-Axis (Letters)
+#define LIMIT_X_RIGHT 21  //right Limit Switch X-Axis (Letters)
+#define LIMIT_Y_UPPER 20  //upper Limit Switch Y-Axis (Numbers)
+#define LIMIT_Y_LOWER 21  //lower Limit Switch Y-Axis (Numbers)
+
+#define X_DISTANCE_TO_A1 30 //Distance in mm from left X-Axis limit switch to field A1
+#define Y_DISTANCE_TO_A1 30 //Distance in mm from lower-left Y-Axis limit switch to field A1
+
 enum Direction{
     Backwards = 0,
     Forwards,
     Left,
     Right
 };
+
+Point currPoint();
 
 void moveByMM(int a_mm, Direction a_dir);
 void moveTo(Point a_desPoint);
@@ -37,6 +47,8 @@ int main()
     gpioSetMode(M2_STEP, PI_OUTPUT);
     gpioSetMode(M2_DIR, PI_OUTPUT);
 
+    calibrate();
+
     moveByMM(10, Direction::Forwards);
 
     return 1;
@@ -46,25 +58,25 @@ void moveByMM(int a_mm, Direction a_dir)
 {
     switch(a_dir)
     {
-        case 0:
+        case 0:     //Backwards
         {
             gpioWrite(M1_DIR, PI_LOW);
             gpioWrite(M2_DIR, PI_HIGH);
             break;
         }
-        case 1:
+        case 1:     //Forwards
         {
             gpioWrite(M1_DIR, PI_HIGH);
             gpioWrite(M2_DIR, PI_LOW);
             break;
         }
-        case 2:
+        case 2:     //Left
         {
             gpioWrite(M1_DIR, PI_HIGH);
             gpioWrite(M2_DIR, PI_HIGH);
             break;
         }
-        case 3:
+        case 3:     //Right
         {
             gpioWrite(M1_DIR, PI_LOW);
             gpioWrite(M2_DIR, PI_LOW);
@@ -91,5 +103,36 @@ void moveTo(Direction a_desPoint)
 
 void calibrate()
 {
-    // move to lower corner until limit switches are triggered then move to closest field
+    // move to lower-left corner until limit switches are triggered then move to closest field (A1)
+    while(gpioRead(LIMIT_X_LEFT) == PI_LOW)
+    {
+        gpioWrite(M1_DIR, PI_HIGH);
+        gpioWrite(M2_DIR, PI_HIGH);
+
+        gpioWrite(M1_STEP, PI_HIGH);
+        gpioWrite(M2_STEP, PI_HIGH);
+        time_sleep(0.001);
+        gpioWrite(M1_STEP, PI_LOW);
+        gpioWrite(M2_STEP, PI_LOW);
+        time_sleep(0.001);
+    }
+
+    while(gpioRead(LIMIT_Y_LOWER) == PI_LOW)
+    {
+        gpioWrite(M1_DIR, PI_LOW);
+        gpioWrite(M2_DIR, PI_HIGH);
+
+        gpioWrite(M1_STEP, PI_HIGH);
+        gpioWrite(M2_STEP, PI_HIGH);
+        time_sleep(0.001);
+        gpioWrite(M1_STEP, PI_LOW);
+        gpioWrite(M2_STEP, PI_LOW);
+        time_sleep(0.001);
+    }
+
+    moveByMM(X_DISTANCE_TO_A1, Direction::Right);
+    moveByMM(Y_DISTANCE_TO_A1, Direction::Forwards);
+
+    Point point(0,7);
+    currPoint = point;
 }
